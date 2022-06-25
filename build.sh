00b06bf1733b0232950f28bede7fb3a5401ba636
Kenta Sato <tosainu.maple@gmail.com>
2022-06-25 11:46:38 +0900

    .circleci/config.yml

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinuxarm
 
 sudo sed -i 's/^#\?\(Color\)/\1/' /etc/pacman.conf
-sudo sed -i 's/^#\?\(MAKEFLAGS=\).*$/\1"-j4"/' /etc/makepkg.conf
+sudo sed -i 's/^#\?\(MAKEFLAGS=\).*$/\1"-j6"/' /etc/makepkg.conf
 sudo pacman -Syyu --noconfirm --noprogressbar
 
 run_makepkg "core/linux-aarch64"
